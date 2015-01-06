94046c1c3b9d5563029b77f55328c5a7028ebd0b
AtoCrafter <ato.crafter@gmail.com>
2015-01-07 00:05:17 +0900

    Add Frame block

--- src/main/scala/ato/formulabuilder/FormulaBuilder.scala
+++ src/main/scala/ato/formulabuilder/FormulaBuilder.scala
@@ -1,13 +1,18 @@
 package ato.formulabuilder
 
+import ato.formulabuilder.block.BlockFrame
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.registry.GameRegistry
 
 @Mod(modid = "FormulaBuilder", modLanguage = "scala")
 object FormulaBuilder {
+
+  val blockFrame = new BlockFrame()
+
   @EventHandler
   def preInit(event: FMLPreInitializationEvent): Unit = {
-    
+    GameRegistry.registerBlock(blockFrame, "Frame")
   }
 }
