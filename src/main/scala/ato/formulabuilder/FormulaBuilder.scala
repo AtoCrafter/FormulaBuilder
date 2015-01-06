bce45a9afa2152ec1112bf1dce17900fa684609b
AtoCrafter <ato.crafter@gmail.com>
2015-01-06 23:42:17 +0900

    first commit

---
+++ src/main/scala/ato/formulabuilder/FormulaBuilder.scala
@@ -0,0 +1,13 @@
+package ato.formulabuilder
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+
+@Mod(modid = "FormulaBuilder", modLanguage = "scala")
+object FormulaBuilder {
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit = {
+    
+  }
+}
