fa81990300feef21d1dec93c7537ebfdd417000d
AtoCrafter <ato.crafter@gmail.com>
2015-01-10 12:50:11 +0900

    Add Recipe

--- src/main/scala/ato/formulabuilder/FormulaBuilder.scala
+++ src/main/scala/ato/formulabuilder/FormulaBuilder.scala
@@ -9,6 +9,8 @@ import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEv
 import cpw.mods.fml.common.network.NetworkRegistry
 import cpw.mods.fml.common.registry.GameRegistry
 import cpw.mods.fml.relauncher.Side
+import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.ItemStack
 
 @Mod(modid = "FormulaBuilder", modLanguage = "scala")
 object FormulaBuilder {
@@ -21,6 +23,14 @@ object FormulaBuilder {
   def preInit(event: FMLPreInitializationEvent): Unit = {
     GameRegistry.registerBlock(blockBuilder, "FormulaBuilder")
     GameRegistry.registerTileEntity(classOf[TileEntityFormulaBuilder], "FormulaBuilder")
+    GameRegistry.addRecipe(new ItemStack(blockBuilder),
+      " B ",
+      "DAD",
+      "OOO",
+      new Character('B'), Items.book,
+      new Character('D'), Items.diamond,
+      new Character('A'), Blocks.beacon,
+      new Character('O'), Blocks.obsidian)
 
     packetHandler.registerMessage(
       classOf[MessageHandler], classOf[Message], 0, Side.SERVER)
